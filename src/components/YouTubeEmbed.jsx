b7eb6842bd4d2e714b07a70f54bc8ff755831f40
saulgoodmanvenus-dot <saulgoodman.venus@gmail.com>
2026-08-26 18:10:33 +0530

    Add files via upload

--- src/components/YouTubeEmbed.jsx
+++ src/components/YouTubeEmbed.jsx
@@ -9,22 +9,6 @@ export default function YouTubeEmbed({ videoId }) {
 
   const handlePlay = () => {
     setLoaded(true);
-
-    // Request fullscreen on mobile devices to improve performance and experience
-    if (window.innerWidth <= 768 && wrapperRef.current) {
-      const elem = wrapperRef.current;
-      try {
-        if (elem.requestFullscreen) {
-          elem.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) {
-          elem.webkitRequestFullscreen();
-        } else if (elem.msRequestFullscreen) {
-          elem.msRequestFullscreen();
-        }
-      } catch (err) {
-        console.error("Error attempting to enable fullscreen:", err);
-      }
-    }
   };
 
   return (
@@ -54,9 +38,9 @@ export default function YouTubeEmbed({ videoId }) {
         </div>
       ) : (
         <iframe
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1&vq=hd1080&playsinline=0`}
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1&hd=1`}
           title="YouTube video"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         />
       )}
